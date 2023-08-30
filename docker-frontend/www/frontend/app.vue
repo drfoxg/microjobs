a74f2297f90cc771693dcaa5da9a069475a712ab
drfoxg <drfoxg@gmail.com>
2023-08-30 13:06:37 +0300

    add: frontend services

--- docker-frontend/www/frontend/app.vue
+++ docker-frontend/www/frontend/app.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Hello World!</h1>
+        <h1>Биржа микрозаданий</h1>
         <!-- Markup shared across all pages, ex: NavBar -->
         <NuxtPage />
     </div>
